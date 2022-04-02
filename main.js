f71b401ab138b23c00759d6b8ec6e7e55860cd62
João Vitor Esposte Campos <102496892+JoaoVitorEsposte@users.noreply.github.com>
2022-04-02 18:28:09 -0300

    Última atualização do dia

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ function tocaSom (idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 	let tecla = listaDeTeclas[contador].classList[1];
 	let idAudio = `#som_${tecla}`;
-
 
 	listaDeTeclas[contador].onclick = function () {
 		tocaSom(idAudio);
 	}
-	contador++;
+
+	tecla.onkeydown = function () {
+		tecla.classList.add('ativa');
+	}
 }
