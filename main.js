5bcf4610f634dd338854449f53e9442b202c1dd5
João Vitor Esposte Campos <102496892+JoaoVitorEsposte@users.noreply.github.com>
2022-04-02 17:24:27 -0300

    Aula "Funções com Parâmetros"

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (){
-	document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio){
+	document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+	listaDeTeclas[contador].onclick = tocaSom;
+	contador++;
+}
