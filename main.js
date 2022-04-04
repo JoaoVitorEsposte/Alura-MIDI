db6ce617e6ece5155932a0e301fecf23c4c1d0a3
João Vitor Esposte Campos <102496892+JoaoVitorEsposte@users.noreply.github.com>
2022-04-03 22:52:57 -0300

    Aula "Eventos do Teclado"
    Nesta aula melhoramos a navegação via teclado do Alura MIDI, porém ainda precisa ser melhorado.

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-function tocaSom (idElementoAudio){
+function tocaSom (idElementoAudio) {
 	document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++){
-	let tecla = listaDeTeclas[contador].classList[1];
-	let idAudio = `#som_${tecla}`;
+	const tecla = listaDeTeclas[contador];
+	const instrumento = tecla.classList[1];
+	const idAudio = `#som_${instrumento}`;
 
-	listaDeTeclas[contador].onclick = function () {
+	tecla.onclick = function () {
 		tocaSom(idAudio);
 	}
 
